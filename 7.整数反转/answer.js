8780df06c21a59615403ca0271cb352c4c67a29a
ravenlynn <593365037@qq.com>
2019-03-13 00:45:29 +0800

    q7

---
+++ 7.整数反转/answer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+// 测试用例
+var x = 1534236469;
+var reverse = function(x) {
+    let flag = x >= 0,
+        [...arr] = x.toString(10),
+        resultWithoutFlag = parseInt(arr.reverse().join(''));
+    
+    if (resultWithoutFlag <= -Math.pow(2, 31) || resultWithoutFlag >= Math.pow(2, 31) - 1 ) {
+        return 0;
+    } else {
+        return flag ? resultWithoutFlag : -resultWithoutFlag;
+    }
+
+};
+console.log(reverse(x))
+// 翻转整数与翻转字符串类比
+// 重复弹出x的最后一位数字，并推入rev的后面。最后rev与x相反
+// pop
+// pop = x % 10;
+// x /= 10;
+// push
+// temp = rev * 10 + pop;
+// rev = temp;
+// 需要判断temp是否溢出，后略
