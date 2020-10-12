817e528b595cde573498fb70848a5b1003c7b032
yanglinbj01 <yanglinbj01@fenbi.com>
2020-10-12 11:28:06 +0800

    ADD: daily No.530 inorder traversal

--- 530.minimum-absolute-difference-in-bst[E]/answer.js
+++ 530.minimum-absolute-difference-in-bst[E]/answer.js
@@ -10,20 +10,27 @@
  * @return {number}
  */
 var getMinimumDifference = function(root) {
-    let treeArr = [];
-    let lastTree;
-    let res = Infinity;
-    while(root || treeArr.length){
-        while(root){
-            treeArr.push(root);
-            root = root.left;
-        }
-        if(treeArr.length){
-            root = treeArr.pop();
-            if(lastTree != undefined) res = Math.min(res,root.val - lastTree);
-            lastTree = root.val;
-            root = root.right;
-        }
+  let ans = Infinity, pre = -1;
+  const dfs = (root) => {
+    if (root === null) {
+      return;
     }
-    return res;
+    dfs(root.left);
+    if (pre === -1) {
+      pre = root.val;
+    } else {
+      ans = Math.min(ans, root.val - pre);
+      pre = root.val;
+    }
+    dfs(root.right);
+  }
+  dfs(root);
+  return ans;
 };
+
+/*
+ * 思路
+ * 考虑对升序数组，求任意两个元素之差的绝对值的最小值，答案已定位相邻两个元素之差的最小值。
+ * 其他任意间隔距离大于等于2的元素之差一定大于距离更近的元素之差。
+ * 二叉搜索树的性质 => 二叉搜索树的中序遍历得到的值序列是递增有序的。
+ */
