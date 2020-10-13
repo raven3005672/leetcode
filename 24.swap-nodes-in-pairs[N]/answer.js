36eb4eb76699cd326cbf1843add165d36f5651fc
yanglinbj01 <yanglinbj01@fenbi.com>
2020-10-13 14:48:37 +0800

    ADD: daily No.23 linked list

--- 24.swap-nodes-in-pairs[N]/answer.js
+++ 24.swap-nodes-in-pairs[N]/answer.js
@@ -10,9 +10,16 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    if (!head || !head.next) return head;
-    let next = head.next;
-    head.next = swapPairs(next.next);
-    next.next = head;
-    return next;
+  if (!head || !head.next) {
+    return head;
+  }
+  let next = head.next;
+  head.next = swapPairs(next.next);
+  next.next = head;
+  return next;
 };
+
+/*
+ * 思路：递归
+ * 两个一组翻转，并连接每个子翻转结构，不足两个的直接返回
+ */
