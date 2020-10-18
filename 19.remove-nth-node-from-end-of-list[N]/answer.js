02eed2b197c8827f4c2c968e2b7e511b7bbd39d2
yanglinbj01 <yanglinbj01@fenbi.com>
2020-10-18 18:52:23 +0800

    MOD: daily No.19 linked list

--- 19.remove-nth-node-from-end-of-list[N]/answer.js
+++ 19.remove-nth-node-from-end-of-list[N]/answer.js
@@ -10,19 +10,25 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
-    if (!head || !head.next) return null;
-    let fast = head, slow = head;
-    for (let i = 0; i < n; i++) {
-        fast = fast.next;
-    }
-    if (!fast) {
-        return head.next;
-    }
-    while (fast.next) {
-        fast = fast.next;
-        slow = slow.next;
-    }
-    slow.next = slow.next.next;
-    return head;
+var removeNthFromEnd = function (head, n) {
+  if (!head || !head.next) return null;
+  let fast = head, slow = head;
+  for (let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
+  if (!fast) {
+    return head.next;
+  }
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+  return head;
 };
+
+/*
+ * 思路：快慢指针
+ * 快指针比慢指针快n，快指针到终点的时候删除慢指针
+ * 时间复杂度：O(n)，空间复杂度：O(1)
+ */
