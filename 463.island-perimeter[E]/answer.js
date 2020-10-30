66d09a73cb811628bcd7c9b395eda41feca3f047
yanglinbj01 <yanglinbj01@fenbi.com>
2020-10-30 19:02:26 +0800

    ADD: daily No.463 & No.721

--- 463.island-perimeter[E]/answer.js
+++ 463.island-perimeter[E]/answer.js
@@ -2,21 +2,32 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var islandPerimeter = function(grid) {
-    var res=0;
-    for(var i=0;i<grid.length;i++){
-        for(var ii=0;ii<grid[0].length;ii++){
-            if(grid[i][ii]==1){
-                if(i-1<0||grid[i-1][ii]==0)
-                    res+=1;
-                if(i+1>=grid.length||grid[i+1][ii]==0)
-                    res+=1;
-                if(ii-1<0||grid[i][ii-1]==0)
-                    res+=1;
-                if(ii+1>=grid[0].length||grid[i][ii+1]==0)
-                    res+=1;
-            }
+var islandPerimeter = function (grid) {
+  const dx = [0, 1, 0, -1];
+  const dy = [1, 0, -1, 0];
+  const n = grid.length, m = grid[0].length;
+  let ans = 0;
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < m; ++j) {
+      if (grid[i][j]) {
+        let cnt = 0;
+        for (let k = 0; k < 4; ++k) {
+          let tx = i + dx[k];
+          let ty = j + dy[k];
+          if (tx < 0 || tx >= n || ty < 0 || ty >= m || !grid[tx][ty]) {
+            cnt += 1;
+          }
         }
+        ans += cnt;
+      }
     }
-    return res;
+  }
+  return ans;
 };
+
+/*
+ * 思路：迭代
+ * 对于一个陆地格子的每条边，它被算作岛屿的周长当且仅当这条边为网格的边界或者相邻的另一个格子为水域。
+ * 因此，我们可以遍历每个陆地格子，看其四个方向是否为边界或者水域，如果是，将这条边的贡献（即 1）加入答案 ans 中即可。
+ * 时间复杂度：O(n*m)，空间复杂度：O(1)
+ */
