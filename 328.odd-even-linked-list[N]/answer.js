20625f6262bee1ad495f352e74a17b3203439a0c
yanglinbj01 <yanglinbj01@fenbi.com>
2020-11-13 11:38:38 +0800

    ADD: daily No.328

---
+++ 328.odd-even-linked-list[N]/answer.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+  if (head === null) {
+      return head;
+  }
+  let evenHead = head.next;
+  let odd = head, even = evenHead;
+  while (even !== null && even.next !== null) {
+      odd.next = even.next;
+      odd = odd.next;
+      even.next = odd.next;
+      even = even.next;
+  }
+  odd.next = evenHead;
+  return head;
+};
+
+/**
+ * 思路：局部翻转链表
+ * 时间复杂度：O(n)，空间复杂度：O(1)
+ */
