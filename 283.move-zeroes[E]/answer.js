4ede86bf0cd1e1086532a259adef9bcb7c0dd1a9
yanglinbj01 <yanglinbj01@fenbi.com>
2020-11-19 23:52:51 +0800

    MOD: daily No.283

--- 283.move-zeroes[E]/answer.js
+++ 283.move-zeroes[E]/answer.js
@@ -2,15 +2,18 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    let count = nums.length;
-    for (let i = 0; i < count;) {
-        if (nums[i] === 0) {
-            nums.splice(i, 1);
-            nums.push(0);
-            count--;
-        } else {
-            i++;
-        }
+var moveZeroes = function (nums) {
+  let n = nums.length, left = 0, right = 0;
+  while (right < n) {
+    if (nums[right]) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left++;
     }
+    right++;
+  }
 };
+
+/**
+ * 思路：双指针
+ * 时间复杂度：O(n)，空间复杂度：O(1)
+ */
