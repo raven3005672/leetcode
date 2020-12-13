5763302ba4026f068bdfc99c6d7ae5138265eb0f
yanglinbj01 <yanglinbj01@fenbi.com>
2020-12-13 23:18:54 +0800

    ADD: daily No.97 & No.217 & No.1476

--- 217.contains-duplicate[E]/answer.js
+++ 217.contains-duplicate[E]/answer.js
@@ -2,14 +2,21 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    let map = {};
-    for (let i = 0; i < nums.length; i++) {
-        if (map[nums[i]]) {
-            return true
-        } else {
-            map[nums[i]] = true;
-        }
+var containsDuplicate = function (nums) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]]) {
+      return true
+    } else {
+      map[nums[i]] = true;
     }
-    return false;
+  }
+  return false;
 };
+
+/**
+ * 思路：排序
+ * 时间复杂度：O(NlogN)，空间复杂度：O(logN)
+ * 思路：哈希
+ * 时间复杂度：O(N)，空间复杂度：O(N)
+ */
