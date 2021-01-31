0327df23ae82344fd3956ca41c46c6c360c688f9
yanglinbj01 <yanglinbj01@fenbi.com>
2021-01-31 22:00:16 +0800

    weekly

--- 674.longest-continuous-increasing-subsequence[E]/answer.js
+++ 674.longest-continuous-increasing-subsequence[E]/answer.js
@@ -2,23 +2,37 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findLengthOfLCIS = function(nums) {
-    //双指针
-    if(nums.length === 0) return 0;
-    if(nums.length === 1) return 1;
-    var res = 1;
-    var count = 1;
-    for(var i=0,j=1;i<nums.length-1&&j<nums.length;){
-        if(nums[i] < nums[j]){
-            i++;
-            j++;
-            count++;
-        }else{
-            res = Math.max(count, res);
-            count = 1;
-            i = j;
-            j = i+1;
-        }
+var findLengthOfLCIS = function (nums) {
+  //双指针
+  if (nums.length === 0) return 0;
+  if (nums.length === 1) return 1;
+  var res = 1;
+  var count = 1;
+  for (var i = 0, j = 1; i < nums.length - 1 && j < nums.length;) {
+    if (nums[i] < nums[j]) {
+      i++;
+      j++;
+      count++;
+    } else {
+      res = Math.max(count, res);
+      count = 1;
+      i = j;
+      j = i + 1;
     }
-    return Math.max(count, res);
+  }
+  return Math.max(count, res);
+};
+
+
+var findLengthOfLCIS = function (nums) {
+  let ans = 0;
+  const n = nums.length;
+  let start = 0;
+  for (let i = 0; i < n; i++) {
+    if (i > 0 && nums[i] <= nums[i - 1]) {
+      start = i;
+    }
+    ans = Math.max(ans, i - start + 1);
+  }
+  return ans;
 };
--- 724.find-pivot-index[E]/answer.js
+++ 724.find-pivot-index[E]/answer.js
@@ -2,17 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var pivotIndex = function(nums) {
-    //复杂度O(n):先计算数组总和，再遍历累加当前元素左侧值与sum进行比较
-    var sum = nums.reduce((accumulator, currentValue)=>accumulator+currentValue,0);
-    var temp = 0;
-    for(var i=0;i<nums.length;i++){
-        if(i>0){
-            temp += nums[i-1];
-        }
-        if(sum - nums[i] === temp * 2){
-            return i;
-        }
+var pivotIndex = function (nums) {
+  //复杂度O(n):先计算数组总和，再遍历累加当前元素左侧值与sum进行比较
+  var sum = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  var temp = 0;
+  for (var i = 0; i < nums.length; i++) {
+    if (i > 0) {
+      temp += nums[i - 1];
     }
-    return -1;
+    if (sum - nums[i] === temp * 2) {
+      return i;
+    }
+  }
+  return -1;
 };
